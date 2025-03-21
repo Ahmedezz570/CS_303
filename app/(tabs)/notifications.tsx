1f6b6ba81ea4e90b4341f430d42b4ca6faf80f33
ramadanesawy <raesawy15@gmail.com>
2025-03-21 06:22:40 +0200

    add the cart page

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -10,3 +10,5 @@ const notifications = () => {
 }
 
 export default notifications
+
+
---
+++ app/CartData.js
@@ -0,0 +1,12 @@
+const datacart=[{
+
+  id: 0,
+  category: 0,
+  name: " ",
+  description: " ",
+  price: 0,
+  image: " "
+}];
+datacart.pop();
+
+export default datacart;
