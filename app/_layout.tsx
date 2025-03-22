1c5e256c66e431c54560148cf32e37ff59d7e94a
ahmedsaeed2515 <ahmedsaeed2515@gmail.com>
2025-03-23 00:26:52 +0200

    add Categories

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,8 +8,16 @@ const Layout = () => {
       <Stack.Screen name="Register"   options={{headerShown:false}}/>
      <Stack.Screen name="ForgetPass" options={{headerShown:false}} />
       {/* <Stack.Screen name="About"  /> */}
-      <Stack.Screen name="singlepage"   options={{ headerShown: false }} 
- />
+      <Stack.Screen name="singlepage"   options={{ headerShown: false }} />
+      <Stack.Screen name="Categories/SeeAllCategories" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/pants" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/mobile" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/dresses" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/jackets" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/t-shirt" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/sweatshirt" options={{ title: "back" }} />
+      <Stack.Screen name="Categories/wedding" options={{ title: "back" }} />
+
       </Stack>
   );
 }
